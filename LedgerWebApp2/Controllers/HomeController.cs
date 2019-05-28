2667b7034b6a6e3fa321e970fcd2a6165e16b2c6
emersone <you@example.com>
2019-05-27 19:43:59 -0700

    Added authorization code

---
+++ LedgerWebApp2/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LedgerWebApp2.Models;
+using LedgerWebApp.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using LedgerApp;
+
+namespace LedgerWebApp2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private Dictionary<String, Account> accountDict = new Dictionary<String, Account>();
+
+
+        [HttpGet]
+        public IActionResult CreateAccount()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateAccount(CreateAccountModel model)
+        {
+            // TODO do work
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            if (LoginUser(loginModel.username, loginModel.password))
+            {
+                var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, loginModel.username)
+            };
+
+                var userIdentity = new ClaimsIdentity(claims, "login");
+
+                ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+                await HttpContext.SignInAsync(principal);
+
+                //This is not the best solution
+                //Recore the current users username as a cookie
+                Response.Cookies.Append("user", loginModel.username);
+
+                //Just redirect to our index after logging in.
+                return Redirect("/");
+            }
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            // TODO add functonality for deposit, withdrawal, check balance, and list transactions.
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private bool LoginUser(string username, string password)
+        {
+            //As an example. This method would go to our data store and validate that the combination is correct. 
+            //For now just return true. 
+            return true;
+        }
+    }
+}
---
+++ LedgerWebApp2/Models/CreateAccountModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LedgerWebApp2.Models
+{
+    public class CreateAccountModel
+    {
+        [Required]
+        public string username { get; set; }
+        [Required]
+        public string password { get; set; }
+        [Required]
+        public string firstName { get; set; }
+        [Required]
+        public string lastName { get; set; }
+    }
+}
